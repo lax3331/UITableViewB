8478753fb913ef99ea1f32b039e5f99aeb7b4509
Nikky <la2life1234@gmail.com>
2023-03-29 01:31:48 +0300

    Initial Commit

---
+++ UITableViewB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableViewB
+//
+//  Created by Nikita Kuznetsov on 29.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
